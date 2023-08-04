a8d9158f35e83607a2e3eeef07ed6984b80bc923
Toseef Ahmad <Toseef.Ahmad@cireson.com>
2023-08-04 12:12:06 +0500

    Telerik component practice

--- BlazorProject/Client/Pages/EmployeeListBase.cs
+++ BlazorProject/Client/Pages/EmployeeListBase.cs
@@ -2,17 +2,42 @@
 using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using Telerik.Blazor;
 
 namespace BlazorProject.Client.Pages
 {
     public class EmployeeListBase : ComponentBase
     {
+        protected StringFilterOperator StringFilterOperator { get; set; }
         protected string message { get; set; }
         protected string FirstNameText { get; set; }
         protected Employee Employee { get; set; }
 
         protected List<Department> DepartmentList { get; set; } = new List<Department>();
         protected string DepartmentIdText { get; private set; }
+        public List<int> SelectedDepartments { get; set; } = new List<int>();
+
+        protected List<Subscriptions> SubscriptionsList { get; set; }
+        protected bool CheckAll
+        {
+            get
+            {
+                return SubscriptionsList.All(x => x.Selected);
+            }
+            set
+            {
+                SubscriptionsList.ForEach(x => x.Selected = value);
+            }
+        }
+
+        protected bool CheckAllIndescriminate
+        {
+            get
+            {
+                return !CheckAll && SubscriptionsList.Any(x=> x.Selected);
+            }
+        }
 
         protected EmployeeListBase()
         {
@@ -21,6 +46,8 @@ namespace BlazorProject.Client.Pages
 
         protected override void OnInitialized()
         {
+            LoadSubsriptions();
+            StringFilterOperator = StringFilterOperator.Contains;
             Employee = new Employee
             {
                 EmployeeId = 1,
@@ -59,6 +86,30 @@ namespace BlazorProject.Client.Pages
             };
         }
 
+        public void LoadSubsriptions()
+        {
+            SubscriptionsList = new List<Subscriptions>() {
+                new Subscriptions()
+                {
+                    Id= 1,
+                    SubscriptionName = "Gold",
+                    Selected = false
+                },
+                new Subscriptions()
+                {
+                    Id= 2,
+                    SubscriptionName = "Silver",
+                    Selected = false
+                },
+                new Subscriptions()
+                {
+                    Id= 3,
+                    SubscriptionName = "Bronze",
+                    Selected = false
+                },
+            };
+        }
+
         protected void FormSubmitCallback()
         {
 
@@ -79,6 +130,16 @@ namespace BlazorProject.Client.Pages
         {
             DepartmentIdText = value.ToString();
             Employee.DepartmentId = value;
+        }
+
+        protected void DepartmentACChangeEvent(object value)
+        {
+            DepartmentIdText = value.ToString();
+            Employee.DepartmentId = DepartmentList.FirstOrDefault(x => x.DepartmentName == DepartmentIdText).DepartmentId;
+        }
+        protected void MultiSelectDepartmentChangeEvent(List<int> value)
+        {
+            SelectedDepartments = value;
         }
     }
 }
---
+++ BlazorProject/Shared/Subscriptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorProject.Shared
+{
+    public class Subscriptions
+    {
+        public int Id { get; set; }
+        public string SubscriptionName { get; set; }
+
+        public bool Selected { get; set; }
+    }
+}
